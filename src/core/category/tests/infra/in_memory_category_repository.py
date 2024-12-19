8215f4571f00329a77ada5c9d30a22473dee2fad
edsonjabastos <edsonjabastos@hotmail.com>
2024-12-19 13:41:38 -0300

    fix: initialize categories list with provided categories or empty list

--- src/core/category/tests/infra/in_memory_category_repository.py
+++ src/core/category/tests/infra/in_memory_category_repository.py
@@ -4,7 +4,7 @@ from core.category.domain.category import Category
 
 class InMemoryCategoryRepository:
     def __init__(self, categories: List[Category] = None) -> None:
-        self.categories: List[Category] = []
+        self.categories: List[Category] = categories or []
 
     def save(self, category: Category) -> None:
         self.categories.append(category)
