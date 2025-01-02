db10bbdee86197e0d64393c02f6954cd60767011
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-02 09:24:55 -0300

    refactor: update DeleteCategory to use CategoryRepository interface

--- src/core/category/application/use_cases/delete_category.py
+++ src/core/category/application/use_cases/delete_category.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from uuid import UUID
 
+from core.category.application.use_cases.category_repository import CategoryRepository
 from core.category.domain.category import Category
 from core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
 from core.category.application.use_cases.exceptions import CategoryNotFound
@@ -12,8 +13,8 @@ class DeleteCategoryRequest:
 
 
 class DeleteCategory:
-    def __init__(self, repository: InMemoryCategoryRepository) -> None:
-        self.repository: InMemoryCategoryRepository = repository
+    def __init__(self, repository: CategoryRepository) -> None:
+        self.repository: CategoryRepository = repository
 
     def execute(self, request: DeleteCategoryRequest) -> None:
         category: Category = self.repository.get_by_id(request.id)
