35c9f21399f548dbc472476df841221b8b5b5c3c
Edson Closed <edson.bastos@closed-loop.net>
2025-01-02 16:35:38 -0300

    feat: implement Django ORM repository for Category model with CRUD operations

--- src/django_project/category_app/repository.py
+++ src/django_project/category_app/repository.py
@@ -1,1 +1,46 @@
+from typing import List
+from uuid import UUID
+
 from core.category.domain.category_repository import CategoryRepository
+from core.category.domain.category import Category
+
+from django_project.category_app.models import Category as CategoryModel
+
+
+class DjangoORMCategoryRepository(CategoryRepository):
+    def __init__(self, category_model: CategoryModel):
+        self.category_model = category_model
+
+    def save(self, category: Category):
+        self.category_model.objects.create(
+            id=category.id,
+            name=category.name,
+            description=category.description,
+            is_active=category.is_active,
+        )
+
+    def get_by_id(self, id: UUID) -> Category:
+        try:
+            category = self.category_model.objects.get(id=id)
+            return Category(
+                id=category.id,
+                name=category.name,
+                description=category.description,
+                is_active=category.is_active,
+            )
+        except self.category_model.DoesNotExist:
+            return None
+
+    def delete(self, id: UUID):
+        self.category_model.objects.filter(id=id).delete()
+
+    def list(self) -> List[Category]:
+        return [
+            Category(
+                id=category.id,
+                name=category.name,
+                description=category.description,
+                is_active=category.is_active,
+            )
+            for category in self.category_model.objects.all()
+        ]
