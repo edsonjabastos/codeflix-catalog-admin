aca81798b6441f84fc5813baa2ae4dde6ba13463
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-02 10:16:55 -0300

    test: add unit tests for handling category update scenarios

--- src/core/category/tests/application/use_cases/unit/test_update_category.py
+++ src/core/category/tests/application/use_cases/unit/test_update_category.py
@@ -1,6 +1,9 @@
 from unittest.mock import create_autospec
 from uuid import uuid4
+
+import pytest
 from core.category.application.use_cases.category_repository import CategoryRepository
+from core.category.application.use_cases.exceptions import CategoryNotFound
 from core.category.application.use_cases.update_category import (
     UpdateCategory,
     UpdateCategoryRequest,
@@ -82,3 +85,62 @@ class TestUpdateCategory:
         assert category.is_active is True
         assert category.id == update_category_request.id
         mock_repository.update.assert_called_once_with(category)
+
+    def test_update_category_not_found(self) -> None:
+        mock_repository: CategoryRepository = create_autospec(CategoryRepository)
+        mock_repository.get_by_id.return_value = None
+
+        use_case: UpdateCategory = UpdateCategory(repository=mock_repository)
+        update_category_request = UpdateCategoryRequest(id=str(uuid4()))
+        with pytest.raises(CategoryNotFound) as e:
+            use_case.execute(update_category_request)
+        assert (
+            str(e.value)
+            == f"Category not found with the given id {update_category_request.id} while updating"
+        )
+
+    def test_update_category_with_name_empty(self) -> None:
+        category: Category = Category(
+            name="Movie", description="Movie description", is_active=True
+        )
+        mock_repository: CategoryRepository = create_autospec(CategoryRepository)
+        mock_repository.get_by_id.return_value = category
+
+        use_case: UpdateCategory = UpdateCategory(repository=mock_repository)
+        update_category_request = UpdateCategoryRequest(id=category.id, name="")
+        with pytest.raises(ValueError) as e:
+            use_case.execute(update_category_request)
+        assert str(e.value) == "name cannot be empty"
+
+    def test_update_category_with_name_longer_than_255(self) -> None:
+        category: Category = Category(
+            name="Movie", description="Movie description", is_active=True
+        )
+        mock_repository: CategoryRepository = create_autospec(CategoryRepository)
+        mock_repository.get_by_id.return_value = category
+
+        use_case: UpdateCategory = UpdateCategory(repository=mock_repository)
+        update_category_request = UpdateCategoryRequest(id=category.id, name="a" * 256)
+        with pytest.raises(ValueError) as e:
+            use_case.execute(update_category_request)
+        assert str(e.value) == "name cannot be longer than 255 characters"
+
+    def test_update_category_name_description_and_is_active(self) -> None:
+        category: Category = Category(
+            name="Movie", description="Movie description", is_active=True
+        )
+        mock_repository: CategoryRepository = create_autospec(CategoryRepository)
+        mock_repository.get_by_id.return_value = category
+
+        use_case: UpdateCategory = UpdateCategory(repository=mock_repository)
+        update_category_request = UpdateCategoryRequest(
+            id=category.id, name="New Movies", description="New Movie description", is_active=False
+        )
+        use_case.execute(update_category_request)
+
+        assert category.name == "New Movies"
+        assert category.description == "New Movie description"
+        assert category.is_active is False
+        assert category.id == update_category_request.id
+        mock_repository.update.assert_called_once_with(category)
+        
