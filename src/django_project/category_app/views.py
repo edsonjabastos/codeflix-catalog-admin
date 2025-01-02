35d9e8107637fc10288052ae80dc07bfe34b4592
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-02 13:44:24 -0300

    feat: implement CategoryViewSet and register API endpoint for categories

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ViewSet):
+
+    def list(self, request: Request) -> Response:
+        return Response(
+            status=HTTP_200_OK,
+            data=[
+                {
+                    "id": "9bcf7e5a-3f4c-4f8a-8f9b-1b7e5f3b7c9b",
+                    "name": "Movie",
+                    "description": "Movie category",
+                    "is_active": True,
+                },
+                {
+                    "id": "9bcf7e5a-3f4c-4f8a-8f9b-1b7e5f3b7c9b",
+                    "name": "Documentary",
+                    "description": "Documentary category",
+                    "is_active": True,
+                },
+            ],
+        )
