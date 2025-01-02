4f7035384d61bfbb6005677bfb32d72a439647a1
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-02 09:24:41 -0300

    feat: add update method to CategoryRepository interface

--- src/core/category/application/use_cases/category_repository.py
+++ src/core/category/application/use_cases/category_repository.py
@@ -15,3 +15,7 @@ class CategoryRepository(ABC):
     @abstractmethod
     def delete(self, id: UUID) -> None:
         raise NotImplementedError
+    
+    @abstractmethod
+    def update(self, category: Category) -> None:
+        raise NotImplementedError
