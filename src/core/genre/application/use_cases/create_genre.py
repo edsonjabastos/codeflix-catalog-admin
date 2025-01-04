86e4222ec653702cc502922c98e6499d93fd9256
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-03 22:12:01 -0300

    feat: enhance CreateGenre use case to handle invalid genre creation and improve category validation

--- src/core/genre/application/use_cases/create_genre.py
+++ src/core/genre/application/use_cases/create_genre.py
@@ -1,8 +1,10 @@
+from math import e
 from typing import Set
 from uuid import UUID
 from dataclasses import dataclass, field
-from core.genre.application.exceptions import RelatedCategoriesNotFound
+from core.genre.application.exceptions import InvalidGenre, RelatedCategoriesNotFound
 from core.category.domain.category_repository import CategoryRepository
+from core.genre.domain.genre import Genre
 from core.genre.domain.genre_repository import GenreRepository
 
 
@@ -24,8 +26,21 @@ class CreateGenre:
         id: UUID
 
     def execute(self, input: Input) -> Output:
-        category_ids = {category.id for category in self.category_repository.list()}
+        category_ids: Set = {
+            category.id for category in self.category_repository.list()
+        }
         if not input.categories.issubset(category_ids):
             raise RelatedCategoriesNotFound(
                 f"Categories with provided IDs not found: {input.categories - category_ids}"
             )
+
+        try:
+            genre: Genre = Genre(
+                name=input.name, is_active=input.is_active, categories=input.categories
+            )
+        except ValueError as error:
+            raise InvalidGenre(str(error))
+
+        self.genre_repository.save(genre)
+
+        return self.Output(id=genre.id)
