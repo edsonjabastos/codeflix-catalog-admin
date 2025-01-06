ebbd069bdc9fac2e2491931ee4b9449c78831bc4
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-06 16:40:34 -0300

    feat: add Genre model with UUID primary key and ManyToMany relationship to categories

--- src/django_project/genre_app/models.py
+++ src/django_project/genre_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Genre(models.Model):
+    app_label = "genre_app"
+
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    categories = models.ManyToManyField("category_app.Category", related_name="genres")
+
+    class Meta:
+        db_table = "genres"
+
+    def __str__(self):
+        return self.name
