03acf7a3244ed0f8f7358818c469deee85080600
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-06 16:41:02 -0300

    fix: update app name to include project namespace

--- src/django_project/genre_app/apps.py
+++ src/django_project/genre_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GenreAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'genre_app'
+    name = 'django_project.genre_app'
