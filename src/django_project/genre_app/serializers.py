d424269552bf2a2fc3f164ff58f794132f9055a6
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-11 22:51:43 -0300

    feat: add serializers for genre creation and response handling

--- src/django_project/genre_app/serializers.py
+++ src/django_project/genre_app/serializers.py
@@ -16,3 +16,22 @@ class GenreGenreOutputSerializer(Serializer):
 
 class ListGenreOutputSerializer(Serializer):
     data: GenreGenreOutputSerializer = GenreGenreOutputSerializer(many=True)
+
+
+# can be done with serializer method field too
+class SetField(ListField):
+    def to_internal_value(self, data):
+        return set(super().to_internal_value(data))
+
+    def to_representation(self, value):
+        return list(super().to_representation(value))
+
+
+class CreateGenreInputSerializer(Serializer):
+    name: CharField = CharField(max_length=255, allow_blank=False)
+    is_active: BooleanField = BooleanField(default=True)
+    categories: SetField = SetField(child=UUIDField())
+
+
+class CreateGenreResponseSerializer(Serializer):
+    id: UUIDField = UUIDField()
