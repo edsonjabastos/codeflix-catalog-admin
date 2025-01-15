2e6a4f15f5d1606fd7c6e1ed238979262ce42a52
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-14 23:57:44 -0300

    feat: implement CastMember model with UUID and type choices

--- src/django_project/castmember_app/models.py
+++ src/django_project/castmember_app/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db.models import TextChoices, UUIDField, CharField, Model
+from uuid import uuid4
 
-# Create your models here.
+from core.castmember.domain.value_objects import CastMemberType
+
+
+# class CastMemberType(TextChoices):
+#     ACTOR: str = "ACTOR", "Actor"
+#     DIRECTOR: str = "DIRECTOR", "Director"
+
+
+class CastMember(Model):
+    app_label: str = "castmember_app"
+
+    id: UUIDField = UUIDField(primary_key=True, default=uuid4)
+    name: CharField = CharField(max_length=255)
+    type: CharField = CharField(
+        max_length=8, choices=[(type.name, type.value) for type in CastMemberType]
+    )
+
+    class Meta:
+        db_table: str = "cast_members"
+
+    def __str__(self):
+        return self.name
