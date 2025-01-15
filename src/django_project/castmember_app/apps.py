0484a3c301cd8b9cd8d314da338ab5ba7d5cd72b
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-15 00:02:38 -0300

    fix: update app name to include project namespace in CastmemberAppConfig

--- src/django_project/castmember_app/apps.py
+++ src/django_project/castmember_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CastmemberAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'castmember_app'
+    name = 'django_project.castmember_app'
