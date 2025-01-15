6458bb8d35f9a07606c51ca89d2643a97366eb63
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-15 00:00:52 -0300

    feat: add serializers for CastMember with type choices and UUID support

---
+++ src/django_project/castmember_app/serializers.py
@@ -0,0 +1,54 @@
+import re
+from rest_framework.serializers import (
+    Serializer,
+    UUIDField,
+    CharField,
+    ChoiceField,
+)
+
+# from django_project.castmember_app.models import CastMemberType
+from core.castmember.domain.value_objects import CastMemberType
+
+
+class CastMemberTypeField(ChoiceField):
+    def __init__(self, **kwargs):
+        # Utilizamos o "choices" do DRF, que permite um conjunto de opções limitado para um certo campo.
+        choices = [(type.name, type.value) for type in CastMemberType]
+        super().__init__(choices=choices, **kwargs)
+
+    def to_internal_value(self, data):
+        # Valor vindo da API como "str" é convertido para o StrEnum
+        return CastMemberType(super().to_internal_value(data))
+
+    def to_representation(self, value):
+        # O valor vindo do nosso domínio é convertido para uma string na API
+        return str(super().to_representation(value))
+
+
+class CastMemberOutputSerializer(Serializer):
+    id: UUIDField = UUIDField()
+    name: CharField = CharField(max_length=255, allow_blank=False)
+    type: CastMemberTypeField = CastMemberTypeField(required=True)
+
+
+class ListCastMemberOutputSerializer(Serializer):
+    data: CastMemberOutputSerializer = CastMemberOutputSerializer(many=True)
+
+
+class CreateCastMemberInputSerializer(Serializer):
+    name: CharField = CharField(max_length=255, allow_blank=False)
+    type: CastMemberTypeField = CastMemberTypeField(required=True)
+
+
+class CreateCastMemberOutputSerializer(Serializer):
+    id: UUIDField = UUIDField()
+
+
+class DeleteCastMemberInputSerializer(Serializer):
+    id: UUIDField = UUIDField()
+
+
+class UpdateCastMemberInputSerializer(Serializer):
+    id: UUIDField = UUIDField(required=True)
+    name: CharField = CharField(max_length=255, allow_blank=False, required=False)
+    type: CastMemberTypeField = CastMemberTypeField(required=True)
