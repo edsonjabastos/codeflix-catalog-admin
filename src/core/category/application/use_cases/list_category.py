9b9e07268f2e4a8ae48b71ab885744fde86321e4
edsonjabastos <edsonjabastos@hotmail.com>
2025-01-17 08:56:05 -0300

    feat: implement pagination and metadata for category listing response

--- src/core/category/application/use_cases/list_category.py
+++ src/core/category/application/use_cases/list_category.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 from uuid import UUID
 from core.category.domain.category_repository import CategoryRepository
 
@@ -14,11 +15,20 @@ class CategoryOutput:
 @dataclass
 class ListCategoryRequest:
     order_by: str = "name"
+    current_page: int = 1
+
+
+@dataclass
+class ListOutputMeta:
+    current_page: int
+    per_page: int
+    total: int
 
 
 @dataclass
 class ListCategoryResponse:
     data: list[CategoryOutput]
+    meta: ListOutputMeta = field(default_factory=ListOutputMeta)
 
 
 class ListCategory:
@@ -27,17 +37,28 @@ class ListCategory:
 
     def execute(self, request: ListCategoryRequest) -> ListCategoryResponse:
         categories = self.repository.list()
+        sorted_categories: List = sorted(
+            [
+                CategoryOutput(
+                    id=category.id,
+                    name=category.name,
+                    description=category.description,
+                    is_active=category.is_active,
+                )
+                for category in categories
+            ],
+            key=lambda category: getattr(category, request.order_by),
+        )
+        DEFAULT_PAGE_SIZE = 2
+        page_offset = (request.current_page - 1) * DEFAULT_PAGE_SIZE
+        categories_page = sorted_categories[
+            page_offset : page_offset + DEFAULT_PAGE_SIZE
+        ]
         return ListCategoryResponse(
-            data=sorted(
-                [
-                    CategoryOutput(
-                        id=category.id,
-                        name=category.name,
-                        description=category.description,
-                        is_active=category.is_active,
-                    )
-                    for category in categories
-                ],
-                key=lambda category: getattr(category, request.order_by),
-            )
+            data=categories_page,
+            meta=ListOutputMeta(
+                current_page=request.current_page,
+                per_page=DEFAULT_PAGE_SIZE,
+                total=len(sorted_categories),
+            ),
         )
