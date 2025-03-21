2dd588e81de3c9b86ae1da0ba0f24d36033290f8
edsonjabastos <edsonjabastos@hotmail.com>
2025-03-21 11:25:38 -0300

    refactor: simplify ListCategory use case by extending ListUseCase and removing redundant classes

--- src/core/category/application/use_cases/list_category.py
+++ src/core/category/application/use_cases/list_category.py
@@ -1,17 +1,10 @@
-from dataclasses import dataclass, field
-from typing import List
+from dataclasses import dataclass
 from uuid import UUID
-from core.category.domain.category_repository import CategoryRepository
+from core._shared.list_use_case import ListUseCase
+from core.category.domain.category import Category
 
 
-class ListCategory:
-    def __init__(self, repository: CategoryRepository):
-        self.repository: CategoryRepository = repository
-
-    @dataclass
-    class Input:
-        order_by: str = "name"
-        current_page: int = 1
+class ListCategory(ListUseCase["Category", "ListCategory.Output"]):
 
     @dataclass
     class Output:
@@ -20,43 +13,5 @@ class ListCategory:
         description: str
         is_active: bool
 
-    @dataclass
-    class OutputMeta:
-        current_page: int
-        per_page: int
-        total: int
-
-    @dataclass
-    class ListOutput:
-        data: list["ListCategory.Output"]
-        meta: "ListCategory.OutputMeta" = field(
-            default_factory="ListCategory.ListOutputMeta"
-        )
-
-    def execute(self, input: Input) -> ListOutput:
-        categories = self.repository.list()
-        sorted_categories: List = sorted(
-            [
-                self.Output(
-                    id=category.id,
-                    name=category.name,
-                    description=category.description,
-                    is_active=category.is_active,
-                )
-                for category in categories
-            ],
-            key=lambda category: getattr(category, input.order_by),
-        )
-        DEFAULT_PAGE_SIZE = 2
-        page_offset = (input.current_page - 1) * DEFAULT_PAGE_SIZE
-        categories_page = sorted_categories[
-            page_offset : page_offset + DEFAULT_PAGE_SIZE
-        ]
-        return self.ListOutput(
-            data=categories_page,
-            meta=ListCategory.OutputMeta(
-                current_page=input.current_page,
-                per_page=DEFAULT_PAGE_SIZE,
-                total=len(sorted_categories),
-            ),
-        )
+    def execute(self, input: "ListCategory.Input") -> "ListUseCase.ListOutput":
+        return super().execute(input, self.Output)
