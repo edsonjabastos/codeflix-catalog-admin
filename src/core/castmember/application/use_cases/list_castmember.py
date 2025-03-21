6e73cfa65b9f3e2748b7347ba19deb0671fffaaf
edsonjabastos <edsonjabastos@hotmail.com>
2025-03-21 11:12:48 -0300

    refactor: simplify ListCastMember use case by extending ListUseCase and updating execute method

--- src/core/castmember/application/use_cases/list_castmember.py
+++ src/core/castmember/application/use_cases/list_castmember.py
@@ -1,19 +1,10 @@
-from typing import List
-from uuid import UUID
 from dataclasses import dataclass
-
+from uuid import UUID
+from core._shared.list_use_case import ListUseCase
 from core.castmember.domain.castmember import CastMember
-from core.castmember.domain.castmember_repository import CastMemberRepository
 
 
-class ListCastMember:
-    def __init__(self, castmember_repository: CastMemberRepository) -> None:
-        self.castmember_repository = castmember_repository
-
-    @dataclass
-    class Input:
-        order_by: str = "name"
-        current_page: int = 1
+class ListCastMember(ListUseCase["CastMember", "ListCastMember.Output"]):
 
     @dataclass
     class Output:
@@ -21,40 +12,5 @@ class ListCastMember:
         name: str
         type: str
 
-    @dataclass
-    class OutputMeta:
-        current_page: int
-        per_page: int
-        total: int
-
-    @dataclass
-    class ListOutput:
-        data: List["ListCastMember.Output"]
-        meta: "ListCastMember.OutputMeta"
-
-    def execute(self, input: Input) -> Output:
-        castmembers: List[CastMember] = self.castmember_repository.list()
-        sorted_castmembers: List = sorted(
-            [
-                self.Output(
-                    id=castmember.id,
-                    name=castmember.name,
-                    type=castmember.type,
-                )
-                for castmember in castmembers
-            ],
-            key=lambda castmember: getattr(castmember, input.order_by),
-        )
-        DEFAULT_PAGE_SIZE = 2
-        page_offset = (input.current_page - 1) * DEFAULT_PAGE_SIZE
-        castmembers_page = sorted_castmembers[
-            page_offset : page_offset + DEFAULT_PAGE_SIZE
-        ]
-        return self.ListOutput(
-            data=castmembers_page,
-            meta=ListCastMember.OutputMeta(
-                current_page=input.current_page,
-                per_page=DEFAULT_PAGE_SIZE,
-                total=len(sorted_castmembers),
-            ),
-        )
+    def execute(self, input: "ListCastMember.Input") -> "ListUseCase.ListOutput":
+        return super().execute(input, self.Output)
