b4fd3451cc79618e963969a3d49d0244b3a5a3e3
edsonjabastos <edsonjabastos@hotmail.com>
2025-04-10 15:51:42 -0300

    feat: add value objects for media ratings and media types

---
+++ src/core/video/domain/value_objects.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from enum import Enum, auto, unique
+from uuid import UUID
+
+@unique
+class Rating(Enum):
+    ER = auto()
+    L = auto()
+    AGE_10 = auto()
+    AGE_12 = auto()
+    AGE_14 = auto()
+    AGE_16 = auto()
+    AGE_18 = auto()
+
+
+@unique
+class MediaStatus(Enum):
+    PENDING = auto()
+    PROCESSING = auto()
+    COMPLETED = auto()
+    ERROR = auto()
+
+@dataclass(frozen=True)
+class ImageMedia:
+    id: UUID
+    checksum: str
+    name: str
+    location: str
+
+
+@dataclass(frozen=True)
+class AudioVideoMedia:
+    id: UUID
+    checksum: str
+    name: str
+    raw_location: str
+    encoded_location: str
+    status: MediaStatus
