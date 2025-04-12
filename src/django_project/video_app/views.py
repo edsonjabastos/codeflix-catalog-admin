cfed72888aebb4aa41ce6ae0fc2245e84b6d1410
edsonjabastos <edsonjabastos@hotmail.com>
2025-04-12 18:28:54 -0300

    feat: enhance VideoViewSet with retrieve method and improve exception handling

--- src/django_project/video_app/views.py
+++ src/django_project/video_app/views.py
@@ -5,22 +5,32 @@ from rest_framework.response import Response
 from core.video.application.use_cases.create_video_without_media import (
     CreateVideoWithoutMedia,
 )
-from core.video.application.exceptions import InvalidVideo, RelatedEntitiesNotFound
+from core.video.application.exceptions import (
+    InvalidVideo,
+    RelatedEntitiesNotFound,
+    VideoNotFound,
+)
 from django_project.video_app.repository import DjangoORMVideoRepository
 from django_project.category_app.repository import DjangoORMCategoryRepository
 from django_project.genre_app.repository import DjangoORMGenreRepository
 from django_project.castmember_app.repository import DjangoORMCastMemberRepository
 
 from rest_framework.status import (
+    HTTP_200_OK,
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
 )
 
 
 from django_project.video_app.serializers import (
     CreateVideoInputSerializer,
     CreateVideoOutputSerializer,
+    GetVideoInputSerializer,
+    GetVideoOutputSerializer,
+    VideoOutputSerializer,
 )
+from core.video.application.use_cases.get_video import GetVideo
 
 
 class VideoViewSet(viewsets.ViewSet):
@@ -31,6 +41,18 @@ class VideoViewSet(viewsets.ViewSet):
         serializer.is_valid(raise_exception=True)
 
         input_data = serializer.validated_data
+
+        # request_data = request.data
+        input_data = {
+            "title": input_data.get("title"),
+            "description": input_data.get("description"),
+            "launch_year": input_data.get("launch_year"),
+            "duration": input_data.get("duration"),
+            "rating": input_data.get("rating"),
+            "categories": input_data.get("categories"),
+            "genres": input_data.get("genres"),
+            "cast_members": input_data.get("cast_members"),
+        }
 
         # Create the Input object for use case
         input_obj = CreateVideoWithoutMedia.Input(
@@ -44,7 +66,6 @@ class VideoViewSet(viewsets.ViewSet):
             cast_members=input_data["cast_members"],
         )
 
-        # Create use case instance
         use_case = CreateVideoWithoutMedia(
             video_repository=DjangoORMVideoRepository(),
             category_repository=DjangoORMCategoryRepository(),
@@ -63,4 +84,24 @@ class VideoViewSet(viewsets.ViewSet):
         return Response(
             status=HTTP_201_CREATED,
             data=CreateVideoOutputSerializer(instance=output).data,
+        )
+
+    def retrieve(self, request: Request, pk: str | None = None) -> Response:
+        serializer: GetVideoInputSerializer = GetVideoInputSerializer(data={"id": pk})
+        serializer.is_valid(raise_exception=True)
+
+        use_case: GetVideo = GetVideo(video_repository=DjangoORMVideoRepository())
+
+        try:
+            result: GetVideo.Output = use_case.execute(
+                input=GetVideo.Input(id=serializer.validated_data["id"])
+            )
+        except VideoNotFound:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+        output: GetVideoOutputSerializer = GetVideoOutputSerializer(instance=result)
+
+        return Response(
+            status=HTTP_200_OK,
+            data=output.data,
         )
