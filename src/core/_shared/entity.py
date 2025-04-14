ba38eb8f80cf379338aafc2b255ade2044731bb5
edsonjabastos <edsonjabastos@hotmail.com>
2025-04-14 13:12:25 -0300

    feat: enhance Entity class with event handling and message bus integration

--- src/core/_shared/entity.py
+++ src/core/_shared/entity.py
@@ -3,12 +3,21 @@ from uuid import UUID, uuid4
 from abc import ABC
 
 from core._shared.notification import Notification
+from core._shared.events.abstract_message_bus import AbstractMessageBus
+from core._shared.events.event import Event
+from core._shared.events.message_bus import MessageBus
 
 
 @dataclass(kw_only=True)
 class Entity(ABC):
     id: UUID = field(default_factory=uuid4)
-    notification: Notification = field(default_factory=Notification)
+    notification: Notification = field(default_factory=Notification, init=False)
+    events: list[Event] = field(default_factory=list, init=False)
+    message_bus: AbstractMessageBus = field(default_factory=MessageBus)
+
+    def dispatch(self, event: Event) -> None:
+        self.events.append(event)
+        self.message_bus.handle(events=self.events)
 
     def __eq__(self, other: "Entity") -> bool:
         print(other, self.__class__)
