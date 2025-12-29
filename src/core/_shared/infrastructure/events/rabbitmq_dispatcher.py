0a17af94ec7e67b4a7d27c3ef35e239b70ce2f98
edsonjabastos <edsonjabastos@hotmail.com>
2025-12-29 15:38:30 -0300

    feat: enhance RabbitMQ event dispatcher to establish connection and declare queue; update integration event string representation

--- src/core/_shared/infrastructure/events/rabbitmq_dispatcher.py
+++ src/core/_shared/infrastructure/events/rabbitmq_dispatcher.py
@@ -1,10 +1,32 @@
+from dataclasses import asdict
+import json
 from core._shared.application.handler import Event
 from core.video.application.events.handlers import EventDispatcher
+from pika import BlockingConnection, ConnectionParameters
+from pika.adapters.blocking_connection import BlockingChannel
 
 
 class RabbitMQEventDispatcher(EventDispatcher):
-    def __init__(self, queue: str = "videos.new") -> None:
+    """ "
+    docker run -d --hostname rabbitmq --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+    """
+
+    def __init__(self, host: str = "localhost", queue: str = "videos.new") -> None:
+        self.host: str = host
         self.queue: str = queue
+        self.connection: BlockingConnection | None = None
+        self.channel: BlockingChannel | None = None
 
     def dispatch(self, event: Event) -> None:
-        print(f"Dispatching event {event} to RabbitMQ")
+        if not self.connection:
+            self.connection: BlockingConnection = BlockingConnection(
+                ConnectionParameters(host=self.host)
+            )
+            self.channel: BlockingChannel = self.connection.channel()
+            self.channel.queue_declare(queue=self.queue)
+        self.channel.basic_publish(
+            exchange="",
+            routing_key=self.queue,
+            body=json.dumps(asdict(event)),
+        )
+        print(f"Dispatching event {event} to RabbitMQ on queue {self.queue}")
--- src/core/video/application/events/integrations_events.py
+++ src/core/video/application/events/integrations_events.py
@@ -7,3 +7,9 @@ from core._shared.events.event import Event
 class AudioVideoMediaUpdatedIntegrationEvent(Event):
     resource_id: str
     file_path: str
+
+    def __str__(self) -> str:
+        return (
+            f"AudioVideoMediaUpdatedIntegrationEvent("
+            f"resource_id={self.resource_id}, file_path={self.file_path})"
+        )
